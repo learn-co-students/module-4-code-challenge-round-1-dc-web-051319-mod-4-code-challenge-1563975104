3ec93aedb05bfe8b9b91c98136e4df216d9e2da1
Peter Viss <peter.c.viss@biola.edu>
2019-07-24 10:57:35 -0400

    level 3 done

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,15 +1,25 @@
 import React from "react";
 import BotCard from "../components/BotCard";
-
+import BotSpecs from "../components/BotSpecs"
 class BotCollection extends React.Component {
-  //your code here
+	state = {
+		isClicked: true
+	}
+
+	changeIsClicked = () => {
+
+	}
 
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {
+				  this.props.bots.map(bot => (<BotCard key={bot.id} bot={bot} addMyBots={this.props.addMyBots} />))
+				 
+			  }
+			  { this.props.bots.map(bot => (<BotSpecs key={bot.id} bot={bot}/>))}
+    		  
     		</div>
   	  </div>
   	);
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -5,12 +5,14 @@ class YourBotArmy extends React.Component {
   //your bot army code here...
 
   render(){
+    console.log(this.props.myBots)
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+            
+    {this.props.myBots.map(bot => { return <BotCard key={bot.id} bot={bot} addMyBots={this.props.takeAwayBots} />})}
+            
           </div>
         </div>
       </div>
