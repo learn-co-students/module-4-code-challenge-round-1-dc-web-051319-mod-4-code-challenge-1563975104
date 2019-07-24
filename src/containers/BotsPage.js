bee9e8f234e360d4219a1403c84b0fcd0da867c8
Robert Pedersen <rob@worldcrimeleague.org>
2019-07-24 10:16:35 -0400

    stupid fetch

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,33 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
+
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+
+  constructor() {
+    super()
+      this.state = {
+        allBots: []
+      }
+  }
+
+
+  componentDidMount() {
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+      .then(resp => resp.json())
+      .then(object => {
+        this.setState( { allBots: object } )
+      })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy />
+        <BotCollection bots={this.state.allBots}/>
+
       </div>
     );
   }
