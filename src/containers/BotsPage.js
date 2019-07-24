ab7596b5aab41df56957034261245451c66cb18d
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-07-24 10:08:51 -0400

    Completed Step 3

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,37 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
+
+const URL = 'https://bot-battler-api.herokuapp.com/api/v1/bots'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  state = {
+    allBots: [],
+    yourBotArmy: [],
+  }
+
+  componentDidMount() {
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(allBots => this.setState({allBots: allBots}))
+  }
+
+  recruitBot = (bot) => {
+    if (!this.state.yourBotArmy.includes(bot)) return this.setState({yourBotArmy: [...this.state.yourBotArmy, bot]})
+  }
+
+  removeBot = (bot) => {
+    let yourBotArmyClone = [...this.state.yourBotArmy]
+    let index = yourBotArmyClone.findIndex(botClone => botClone.id === bot.id)
+    yourBotArmyClone.splice(index, 1)
+    this.setState({yourBotArmy: yourBotArmyClone})
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy removeBot={this.removeBot} yourBotArmy={this.state.yourBotArmy}/>
+        <BotCollection recruitBot={this.recruitBot} allBots={this.state.allBots}/>
       </div>
     );
   }
